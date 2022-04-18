c2cdb11a41ac18da0c38e2b424bcec0deccc39a1
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-04-18 17:36:31 +0200

    Create program to send user's keywords into the database.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,36 @@
 
 namespace App\Controller;
 
+use App\Model\HomeManager;
+
 class HomeController extends AbstractController
 {
     /**
-     * Display home page
+     * Display home page and send keywords to Database
      */
     public function index(): string
     {
-        return $this->twig->render('Home/index.html.twig');
+        $errors = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $keywords = trim($_POST['keywords']);
+            $keywords = preg_replace('/\s\s+/', ' ', $keywords);
+            $keywords = explode(' ', $keywords);
+
+            foreach ($keywords as $keyword) {
+                if (empty($keyword)) {
+                    $errors[] = "Merci de renseigner au minimum un mot dans le champ de recherche.";
+                }
+                if (strlen($keyword) > 80) {
+                    $errors[] = "Le mot : \"$keyword\" est trop long. Longueur maximale autorisée : 80 caractères.";
+                }
+            }
+            if (empty($errors)) {
+                $homeManager = new HomeManager();
+                $homeManager->sendToDatabase($keywords);
+                header('Location: search');
+            }
+        }
+        return $this->twig->render('Home/index.html.twig', ['errors' => $errors]);
     }
 }
