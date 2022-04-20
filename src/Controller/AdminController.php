c5296ba5dd65bd57e74fe6f8132268f2e23d7655
Yannis699 <yannis.cristini@gmail.com>
2022-04-20 17:15:59 +0200

    test

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace App\Controller;
+use App\Model\AdminManager;
+use App\Service\UtilityService;
+class AdminController extends AbstractController
+{
+    /**
+     * Add a new item
+     */
+    public function add(): ?string
+    {
+        return $this->twig->render('Admin/add.html.twig');
+    }
+}
+
+
+
+
+
+
+
+
+
