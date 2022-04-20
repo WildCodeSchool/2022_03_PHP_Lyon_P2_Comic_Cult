a9b711f9129f402ee021a54e6a0b5184371957f2
Yannis699 <yannis.cristini@gmail.com>
2022-04-20 17:07:57 +0200

    test

--- src/routes.php
+++ src/routes.php
@@ -13,4 +13,7 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
+
+    'admin/add' => ['AdminController', 'add'],
 ];
+
