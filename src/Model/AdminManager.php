5dc0acf783fe44ff3cd9194511b2bfb58a8e4330
HOURDOU DAVID <davidhourdou@gmail.com>
2022-04-21 10:14:30 +0200

    suppression de la fonction selectAll héritée de AbstractManager

--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -5,16 +5,4 @@ namespace App\Model;
 class AdminManager extends AbstractManager
 {
     public const TABLE = 'comic_book';
-    /**
-     * Get all row from database.
-     */
-    public function selectAll(string $orderBy = '', string $direction = 'ASC'): array
-    {
-        $query = 'SELECT * FROM ' . static::TABLE;
-        if ($orderBy) {
-            $query .= ' ORDER BY ' . $orderBy . ' ' . $direction;
-        }
-
-        return $this->pdo->query($query)->fetchAll(\PDO::FETCH_ASSOC);
-    }
 }
