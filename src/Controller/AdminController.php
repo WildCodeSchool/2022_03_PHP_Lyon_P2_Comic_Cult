43eb9f35cf6d037265ed6276fb77e49a1cdbea85
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-04-21 15:28:31 +0200

    AdminController -> Find a way to display errors in a simple array

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -24,9 +24,23 @@ class AdminController extends AbstractController
 
         if (($_SERVER['REQUEST_METHOD'] === 'POST')) {
             $errors = [];
+            //$arrayOfErrors = [];
             $comicBook = array_map('trim', $_POST);
             // Three function in UtilityService to clean comic book's datas.
             $errors[] = $cleanComicBook->comicBookEmptyVerify($comicBook);
+            $errors[] = $cleanComicBook->comicBookEmptyVerify($comicBook);
+            var_dump($errors);
+            /*
+            foreach ($errors as $key => $value) {
+                foreach ($value as $line) {
+                    if (!empty($line)) {
+                        $arrayOfErrors[] = $line;
+                    }
+                }
+            }
+
+            var_dump($arrayOfErrors);*/
+            //die();
             $errors[] = $cleanComicBook->comicBookNumberValidate($comicBook);
             $errors[] = $cleanComicBook->comicBookStringVerify($comicBook);
             $comicBook['keywords'] = $cleanComicBook->clearString($comicBook['pitch']);
@@ -36,73 +50,11 @@ class AdminController extends AbstractController
             $comicBook['cover'] = $uploadFile;
             // Function to verify integrity of uploaded file.
             $errors[] = $cleanComicBook->coverIntegrityVerify($_FILES);
-            var_dump($errors);
+
 
             /*
-            if (empty($errors)) {
+            if (empty($arrayOfErrors)) {
                 move_uploaded_file($_FILES['avatar']['tmp_name'], $uploadFile);
-            }*/
-
-            /*
-            $uploadDir = 'assets/images/comicUpload/';
-            $uploadFile = $uploadDir . uniqid() . '-' . basename($_FILES['cover']['name']);
-            $comicBook['cover'] = $uploadFile;
-            $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
-            $authorizedExtensions = ['jpg','jpeg','png'];
-            $maxFileSize = 2000000;
-
-            if ((!in_array($extension, $authorizedExtensions))){
-                $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
-            }
-            if (file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize) {
-            $errors[] = "Votre fichier doit faire moins de 2M !";
-            }
-            */
-
-            //var_dump($comicBook);
-            /*
-            if (empty($comicBook['title'])) {
-                $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-            }
-            if (empty($comicBook['date_of_release'])) {
-                $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-            }
-            if (empty($comicBook['category'])) {
-                $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-            }
-            if (empty($comicBook['author_name'])) {
-                $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-            }
-            if (empty($comicBook['pitch'])) {
-                $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-            }*/
-            /*
-            if (
-                empty($comicBook['title']) || empty($comicBook['date_of_release']) ||
-                empty($comicBook['category']) || empty($comicBook['author_name'])  ||
-                empty($comicBook['pitch'])
-                ) {
-                    $errors[] = 'Les champs munis d\'un "*" sont obligatoires.';
-                }*/
-            /*
-            if ($comicBook['title'] > 255) {
-                $errors[] = 'Le titre ne doit pas dépasser 255 caractères.';
-            }*/
-            /*
-            if (!filter_var($comicBook['isbn'], FILTER_VALIDATE_INT)) {
-                $errors[] = 'L\'ISBN est obligatoirement composé de chiffres.';
-            }*/
-            /*
-            if ($comicBook['isbn'] < 9780000000000 && $comicBook['isbn'] > 9799999999999) {
-                $errors[] = 'L\'ISBN est composé de 13 chiffres commençant par 978 ou 979.';
-            }*/
-            /*
-            if ($comicBook['author_name'] > 100) {
-                $errors[] = 'Le champ "Auteur" ne doit pas dépasser 100 caractères.';
-            }*/
-            /*
-            if (!filter_var($comicBook['volume'], FILTER_VALIDATE_INT)) {
-                $errors[] = 'Le nombre de tomes doit être composé de chiffres.';
             }*/
         }
 
