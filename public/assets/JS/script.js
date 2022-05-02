f9ccd564470069ee78cd44911bf00a67b17b51b2
Yannis699 <yannis.cristini@gmail.com>
2022-05-02 12:20:40 +0200

    ajout de tout ce qui est relartif à la page détail et au js, ancienne branche

--- public/assets/JS/script.js
+++ public/assets/JS/script.js
@@ -1,3 +1,4 @@
+//Button go to the top.
 
 window.onscroll = function () {
 	if (window.scrollY<1) 
@@ -5,5 +6,33 @@ window.onscroll = function () {
 			document.getElementById("scrollup_img").style.display = "none";
 		} else {
 			document.getElementById("scrollup_img").style.display = "block";
+		}
+	}
+
+//function for the accordion on the details' view
+
+window.onload = () => {
+		
+	const allAccordionElement = document.querySelectorAll(".accordion-element");
+	const firstActive = document.querySelector(".accordion .active");
+		
+		let section = firstActive.children[1].querySelector("p");
+		let sectionHeight = section.offsetHeight + 20;
+	
+		firstActive.children[1].style.height = sectionHeight+"px";
+	
+		for(let element of allAccordionElement){
+			element.addEventListener("click", function(){
+
+				const active = document.querySelector(".accordion .active");
+				active.classList.remove("active");
+				active.children[1].style.height = 0;
+				this.classList.add("active");
+
+				let section = this.children[1].querySelector("p");
+				let sectionHeight = section.offsetHeight + 20;
+			
+				this.children[1].style.height = sectionHeight+"px";
+			});
 		}
 	}
