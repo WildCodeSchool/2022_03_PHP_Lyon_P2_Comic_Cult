e6afba67f5a9bbf00297c467746254a53e7d6c25
vincentDubresson <vincent-dubresson_student2022@wilder.school>
2022-05-10 12:41:58 +0200

    split script.js in two files because of js errors in the console

---
+++ public/assets/JS/button_script.js
@@ -0,0 +1,10 @@
+//Button go to the top.
+
+window.onscroll = function () {
+	if (window.scrollY<1) 
+		{
+			document.getElementById("scrollup_img").style.display = "none";
+		} else {
+			document.getElementById("scrollup_img").style.display = "block";
+		}
+	}
